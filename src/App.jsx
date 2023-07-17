5b05300cfa34f4a993f0416ebaeb250beb48d406
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-17 16:00:34 +0100

    react router install

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import { Home, ArticlePage, Users } from './pages'
+import "./App.css";
 
 function App() {
-
   return (
     <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/article" element={<ArticlePage />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
