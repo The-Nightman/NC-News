73a439e6901ae7c740d7458336a4328cf0cad736
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-17 16:11:46 +0100

    add components boilerplate and index

---
+++ src/components/Articles-container.jsx
@@ -0,0 +1,5 @@
+const ArticlesContainer = () => {
+  return <></>;
+};
+
+export default ArticlesContainer;
---
+++ src/components/Users-container.jsx
@@ -0,0 +1,5 @@
+const UsersContainer = () => {
+  return <></>;
+};
+
+export default UsersContainer;
