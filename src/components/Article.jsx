e703d0c94a9271854cb8871cd0292817e0d2153e
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-18 12:20:50 +0100

    add article component data

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,5 +1,34 @@
-const Article = () => {
-  return <></>;
+const Article = ({ article }) => {
+  const parseDate = () => {
+    return new Date().toISOString().split("T", 1)[0];
+  };
+
+  return (
+    <>
+      <section className="article-container">
+        <article>
+          <div className="article-header-container">
+            <div className="article-header">
+              <h2>{article.title}</h2>
+              <p>{article.author}</p>
+              <p>{article.topic}</p>
+            </div>
+            <p>{parseDate(article.created_at)}</p>
+          </div>
+          <img
+            src={article.article_img_url}
+            alt={article.title}
+            className="article-image"
+          />
+          <p>{article.body}</p>
+          <div className="article-footer">
+            <a>comments: {article.comment_count}</a>
+            <p>votes: {article.votes}</p>
+          </div>
+        </article>
+      </section>
+    </>
+  );
 };
 
 export default Article;
