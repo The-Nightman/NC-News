4473b4f96e043c2f632121d2add12221b6b306d9
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-18 12:18:50 +0100

    add article page api fetch

--- src/pages/Article-Page.jsx
+++ src/pages/Article-Page.jsx
@@ -1,5 +1,29 @@
+import { Article } from "../components";
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios"
+
 const ArticlePage = () => {
-  return <></>;
+  const { article_id } = useParams();
+  const [article, setArticle] = useState({});
+
+  useEffect(() => {
+    axios
+      .get(`https://nc-news-v54w.onrender.com/api/articles/${article_id}`)
+      .then((res) => {
+        console.log(res.data.article)
+        setArticle(res.data.article)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <>
+      <Article article={article}/>
+    </>
+  );
 };
 
 export default ArticlePage;
