7184a0e8c8561417ccf548f9c8d98fe9c371bbdc
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-18 15:35:26 +0100

    add comments cards child component and conditional rendering logic

--- src/pages/Article-Page.jsx
+++ src/pages/Article-Page.jsx
@@ -1,12 +1,18 @@
-import { Article, Loader } from "../components";
-import { useParams } from "react-router-dom"
+import { Article, Loader, CommentCard } from "../components";
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
-import { getArticleByID } from "../utils/api";
+import { getArticleByID, getArticleComments } from "../utils/api";
 
 const ArticlePage = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
+  const [comments, setComments] = useState({});
+  const [showComments, setShowComments] = useState(false);
   const [loading, setLoading] = useState(true);
+
+  const openComments = () => {
+    setShowComments(!showComments);
+  };
 
   useEffect(() => {
     getArticleByID(article_id)
@@ -19,10 +25,38 @@ const ArticlePage = () => {
       });
   }, []);
 
+  useEffect(() => {
+    getArticleComments(article_id)
+      .then((data) => {
+        setComments(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <>
-    { loading ? <Loader/> :
-      <Article article={article}/> }
+      {loading ? (
+        <Loader />
+      ) : (
+        <Article article={article} openComments={openComments} />
+      )}
+      {showComments ? (
+        <div className="comments">
+          {comments.map(({ comment_id, author, created_at, votes, body }) => {
+            return (
+              <CommentCard
+                key={comment_id}
+                author={author}
+                created_at={created_at}
+                body={body}
+                votes={votes}
+              />
+            );
+          })}
+        </div>
+      ) : null}
     </>
   );
 };
