8eaf2033274da0de56c65da342eac902900f9b8e
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-19 09:42:53 +0100

    add comments container

---
+++ src/components/Comments-container.jsx
@@ -0,0 +1,5 @@
+const CommentsContainer = () => {
+  return <></>;
+};
+
+export default CommentsContainer;
--- src/components/index.js
+++ src/components/index.js
@@ -4,8 +4,9 @@ import Article from './Article'
 import ArticleCard from './Article-card'
 import ArticlesContainer from './Articles-container'
 import CommentCard from './Comment-cards'
+import CommentsContainer from './Comments-container'
 import UserCard from './User-card'
 import UsersContainer from './Users-container'
 import Loader from './loader'
 
-export { Header, Menu, Article, ArticleCard, ArticlesContainer, CommentCard, UserCard, UsersContainer, Loader };
+export { Header, Menu, Article, ArticleCard, ArticlesContainer, CommentCard, UserCard, UsersContainer, Loader, CommentsContainer };
