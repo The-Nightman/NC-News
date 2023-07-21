b283856f22498490cbec6fcb5f4a2da98cf675e1
Keenan Brant <keenan.brant@yahoo.co.uk>
2023-07-21 10:59:47 +0100

    replace menu button with mui component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,19 @@
+import MenuOpenOutlinedIcon from "@mui/icons-material/MenuOpenOutlined";
+import { IconButton } from '@mui/material';
+import { Menu } from "../components";
+
 const Header = () => {
   return (
     <>
       <header className="app-header">
         <h1>NC News</h1>
         <div className="menu-button">
-          <button>menu</button>
+          <IconButton aria-label="open/close menu" size="large">
+            <MenuOpenOutlinedIcon fontSize="large"/>
+          </IconButton>
         </div>
       </header>
+      <Menu/>
     </>
   );
 };
